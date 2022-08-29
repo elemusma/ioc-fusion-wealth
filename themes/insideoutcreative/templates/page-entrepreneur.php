cc526ede248c9d833b04ccf0a22f24a6a6693a4d
Efrain Lemus <efraintlemusm@gmail.com>
2022-08-29 10:48:39 -0400

    add banner section

--- themes/insideoutcreative/templates/page-entrepreneur.php
+++ themes/insideoutcreative/templates/page-entrepreneur.php
@@ -76,8 +76,77 @@ echo '</section>';
 // end of intro
 
 // start of banner
+echo '<section class="position-relative section-intro text-center" style="padding:100px 0;">';
 
+
+
+// echo '<div class="container">';
+// echo '<div class="row">';
+echo '<div class="col-12">';
+
+echo '<div class="position-relative pt-5 pb-5">';
+
+echo wp_get_attachment_image(2998,'full','',['class'=>'w-100 h-100 position-absolute','style'=>'top:0;left:0;object-fit:cover;']);
+
+echo '<h2 class="text-white position-relative">Top Ten Tips Before <br><strong>SELLING YOUR BUSINESS</strong></h2>';
+
+echo '<a href="#" class="btn btn-effect text-white bg-accent d-inline-block position-relative overflow-h">';
+echo '<div class="position-absolute w-100 h-100 bg-accent-secondary" style="top:0;left:-100%;"></div>';
+echo '<span class="position-relative">REQUEST NOW</span>';
+echo '</a>';
+
+echo '</div>';
+echo '</div>';
+// echo '</div>';
+// echo '</div>';
+echo '</section>';
 // end of banner
+
+// start of intro
+
+echo '<section class="position-relative section-intro text-center" style="padding:100px 0;">';
+
+echo '<div class="container">';
+echo '<div class="row">';
+echo '<div class="col-12 pb-4">';
+
+echo '<h2 class="">CEO Founders & Entrepreneurs</h2>';
+echo '<h3 class="">You Face Unique Planning Challenges.</h3>';
+
+echo '</div>';
+
+echo '<div class="col-md-4">';
+echo '<h4>$27B</h4>';
+echo '<div class="divider-small"></div>';
+echo '<span class="text-gray">Assets under management</span>';
+echo '</div>';
+
+echo '<div class="col-md-4">';
+echo '<h4>10</h4>';
+echo '<div class="divider-small"></div>';
+echo '<span class="text-gray">True Fiduciary® Standards</span>';
+echo '</div>';
+
+echo '<div class="col-md-4">';
+echo '<h4>100</h4>';
+echo '<div class="divider-small"></div>';
+echo '<span class="text-gray">Liquidity Plan Events</span>';
+echo '</div>';
+
+echo '<div class="col-12 pt-4">';
+
+echo '<a href="#" class="btn btn-effect text-white bg-accent d-inline-block position-relative overflow-h">';
+echo '<div class="position-absolute w-100 h-100 bg-accent-secondary" style="top:0;left:-100%;"></div>';
+echo '<span class="position-relative">Talk to Founder</span>';
+echo '</a>';
+
+echo '</div>';
+
+echo '</div>';
+echo '</div>';
+echo '</section>';
+
+// end of intro
 
  get_footer();
  ?>
