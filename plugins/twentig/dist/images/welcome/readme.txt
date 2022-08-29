2b8b9331ed80fad48ec06ade01738e0fba65d904
Efrain Lemus <efraintlemusm@gmail.com>
2022-08-29 14:24:42 -0400

    delete unused plugin

--- plugins/twentig/dist/images/welcome/readme.txt
+++
@@ -1,4 +0,0 @@
-This plugin bundles the following third-party resources:
-
-Sensa Emoji https://sensa.co/emoji/
-License: MIT.
--- plugins/twentig/dist/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-edit-site', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-plugins', 'wp-primitives', 'wp-token-list', 'wp-url'), 'version' => '75fec19f619acfe1994e');
