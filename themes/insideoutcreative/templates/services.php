109a1f385030bb6e5a060c8f2dfcc08e04a275c9
Efrain Lemus <efraintlemusm@gmail.com>
2022-09-06 12:47:37 -0400

    update service pages to new layout

--- themes/insideoutcreative/templates/services.php
+++ themes/insideoutcreative/templates/services.php
@@ -14,9 +14,9 @@ $options = get_sub_field('options');
 
 
 
-echo '<section class="pt-5 pb-5 position-relative intro">';
+// echo '<section class="pt-5 pb-5 position-relative intro">';
 
-echo '<div class="container">';
+// echo '<div class="container">';
 
 
 if($options == 'Content Center'){
@@ -24,6 +24,10 @@ if($options == 'Content Center'){
   if(have_rows('content_center')): while(have_rows('content_center')): the_row();
   $content = get_sub_field('content');
   
+  echo '<section class="pt-5 pb-5 position-relative intro">';
+
+echo '<div class="container">';
+
 echo '<div class="row justify-content-center">';
 echo '<div class="col-md-7 text-center pb-5">';
 echo $content;
@@ -46,6 +50,8 @@ endif;
 
 
 echo '</div>';
+echo '</div>';
+echo '</section>';
 endwhile; endif;
 // end of group
 
@@ -55,27 +61,43 @@ endwhile; endif;
 $leftRight = get_sub_field('image_left_or_right');
 $image = get_sub_field('image');
 $content = get_sub_field('content');
+$classes = get_sub_field('classes');
+$overlayBackground = get_sub_field('overlay_background');
 
+// echo '<section class="pt-5 pb-5 position-relative intro" style="background:#f7f7f7;">';
+
+if($image){
+  echo '<section class="pt-5 pb-5 position-relative section-intro ' . $classes . '" style="background:url(' . $image['url'] . ');background-size:cover;">';
+} else {
+  echo '<section class="pt-5 pb-5 position-relative section-intro ' . $classes . '" style="">';
+  // echo '</section>';
+}
+
+echo '<div class="position-absolute w-100 h-100" style="top:0;left:0;background:' . $overlayBackground . '"></div>';
+
+echo '<div class="container">';
 
 if($leftRight == 'Left'){
   echo '<div class="row justify-content-center row-content-image">';
 } elseif ($leftRight == 'Right'){
   echo '<div class="row justify-content-center row-content-image flex-md-row-reverse">';
 }
-  echo '<div class="col-md-6">';
-  echo '<div class="position-relative w-100 h-100">';
+  // echo '<div class="col-md-6">';
+  // echo '<div class="position-relative w-100 h-100">';
 
 
-  echo wp_get_attachment_image($image['id'],'full','',['class'=>'position-absolute h-100 w-100 d-md-block d-none','style'=>'top:0;left:0;object-fit:cover;']);
-  echo wp_get_attachment_image($image['id'],'full','',['class'=>'position-relative h-auto w-100 d-md-none d-block','style'=>'']);
-  echo '</div>';
-  echo '</div>';
+  // echo wp_get_attachment_image($image['id'],'full','',['class'=>'position-absolute h-100 w-100 d-md-block d-none','style'=>'top:0;left:0;object-fit:cover;']);
+  // echo wp_get_attachment_image($image['id'],'full','',['class'=>'position-relative h-auto w-100 d-md-none d-block','style'=>'']);
+  // echo '</div>';
+  // echo '</div>';
 
-  echo '<div class="col-md-6 pt-5 pb-5">';
+  echo '<div class="col-12 pt-5 pb-5">';
   echo $content;
   echo '</div>';
 
   echo '</div>';
+  echo '</div>';
+  echo '</section>';
 endwhile; endif;
 // end of group
 }
@@ -87,8 +109,8 @@ endwhile; endif;
 
 
 
-echo '</div>';
-echo '</section>';
+// echo '</div>';
+// echo '</section>';
 
 endwhile; endif;
 // end of intro
