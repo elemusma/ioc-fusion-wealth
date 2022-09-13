385f0e19860fe87b8e12a55d7a7d5905d9e57897
Efrain Lemus <efraintlemusm@gmail.com>
2022-09-13 13:07:25 -0400

    add blog page using index.php instead of a template

--- themes/insideoutcreative/templates/blog.php
+++ themes/insideoutcreative/templates/blog.php
@@ -10,9 +10,9 @@ $title = get_sub_field('title');
 $content = get_sub_field('content');
 $code = get_sub_field('code');
 
-echo '<section class="position-relative section-header" style="padding:100px 0;background:url(' . wp_get_attachment_image_url($bgImg['id'], 'full') . ');background-size:120%;background-position:top;background-attachment:fixed;background-repeat:no-repeat;">';
+echo '<section class="position-relative section-header" style="padding:100px 0;background:url(' . wp_get_attachment_image_url($bgImg['id'], 'full') . ');background-size:cover;background-position:top;background-attachment:fixed;background-repeat:no-repeat;">';
 
-echo '<div class="position-absolute w-100 h-100" style="top:0;left:0;mix-blend-mode:multiply;background-color: #d2d2d2;"></div>';
+// echo '<div class="position-absolute w-100 h-100" style="top:0;left:0;mix-blend-mode:multiply;background-color: #d2d2d2;"></div>';
 
 
 echo '<div class="container">';
@@ -42,22 +42,31 @@ echo '<div class="col-12 pb-4">';
   echo '<h1 class="ml2">' . get_the_title() . '</h1>';
   
   if ( have_posts() ) : while ( have_posts() ) : the_post();
-the_content();
-endwhile; else:
-echo '<p>Sorry, no posts matched your criteria.</p>';
-endif;
+    the_content();
+    endwhile; else:
+    echo '<p>Sorry, no posts matched your criteria.</p>';
+  endif;
+
 echo '</div>';
 echo '<div class="col-12">';
 echo '<div class="row">';
 
+$blogPageQuery = new WP_Query(array(
+  'posts_per_page'=>10,
+  // 'post_parent'=>$post->ID,
+  'post_type'=>'post',
+  // 'category_name'=>'politics',
+  // 'date_query' => array(
+  //     array(
+  //       'after' => 'Today',
+  //       'inclusive' => true,
+  //       )
+  //     ),
+));
+while($blogPageQuery->have_posts()){
+  $blogPageQuery->the_post(); 
 
-// the query to set the posts per page to 3
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$args = array('posts_per_page' => 10, 'post_type' => 'post', 'paged' => $paged );
-query_posts($args);
-// the loop
-if ( have_posts() ) : while (have_posts()) : the_post();
-// start of col
+  // start of col
 echo '<div class="col-md-6 pr-lg-5 col-blog text-white" style="margin-bottom: 50px;">';
 
     
@@ -88,17 +97,78 @@ echo '</a>';
 
   echo '</div>';
 // end of col
-endwhile;
-// pagination
-echo '<div class="col-md-12 text-center pt-5 pb-5">';
 
-echo paginate_links(array(
-'show_all' => true,
-'prev_text' => '&#60;',
-'next_text' => '&#62;'
-));
-echo '</div>';
-endif;
+// echo pagination_nav();
+
+
+
+  } wp_reset_postdata();
+
+  echo '<div class="col-md-12 text-center pt-5 pb-5">';
+    // echo paginate_links(array(
+    //   'show_all' => true,
+    //   'prev_text' => '&#60;',
+    //   'next_text' => '&#62;'
+    // ));
+    the_posts_pagination( array(
+      'mid_size' => 2,
+      'prev_text' => __( 'Previous Page', 'textdomain' ),
+      'next_text' => __( 'Next Page', 'textdomain' ),
+      ) );
+  echo '</div>';
+
+
+// // the query to set the posts per page to 3
+// $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+// $args = array('posts_per_page' => 10, 'post_type' => 'post', 'paged' => $paged );
+// // query_posts($args);
+// // the loop
+// if ( have_posts() ) : 
+//   while (have_posts()) : the_post();
+// // start of col
+// echo '<div class="col-md-6 pr-lg-5 col-blog text-white" style="margin-bottom: 50px;">';
+
+    
+//     echo '<div class="w-100 h-100 d-flex align-items-end justify-content-center blog-content position-relative overflow-h">';
+//     the_post_thumbnail('full',array('class'=>'position-absolute w-100 h-100'));
+// echo '<div>';
+
+//   echo '<div class="overlay position-absolute"></div>';
+//   echo '<div class="position-relative z-1" style="padding: 150px 25px 50px;">';
+//   echo '<a href="' . get_the_permalink() . '">';
+//   echo '<h3 class="h4">' . get_the_title() . '</h3>';
+// echo '</a>';
+
+//   echo '<hr class="border-white">';
+
+
+//   echo '<p class="">' . get_the_tags('Tags: ') . '</p>';
+//   echo '<a href="' . get_the_permalink() . '">Read More</a>';
+//   echo '</div>';
+
+
+
+//   echo '</div>';
+
+
+//   echo '</div>';
+
+
+//   echo '</div>';
+// // end of col
+//   endwhile;
+// endif;
+// // pagination
+// echo '<div class="col-md-12 text-center pt-5 pb-5">';
+
+// // echo paginate_links(array(
+// //   'show_all' => true,
+// //   'prev_text' => '&#60;',
+// //   'next_text' => '&#62;'
+// // ));
+// echo '</div>';
+// // endif;
+
 echo '</div>';
 echo '</div>';
 
@@ -107,7 +177,34 @@ echo '</div>';
 echo '</section>';
 
 
+if(have_rows('blog_banner')): while(have_rows('blog_banner')): the_row();
+$bgImg = get_sub_field('background_image');
+$title = get_sub_field('title');
+$content = get_sub_field('content');
+$code = get_sub_field('code');
 
+echo '<section class="position-relative section-header" style="padding:100px 0;background:url(' . wp_get_attachment_image_url($bgImg['id'], 'full') . ');background-size:cover;background-position:top;background-attachment:fixed;background-repeat:no-repeat;">';
+
+// echo '<div class="position-absolute w-100 h-100" style="top:0;left:0;mix-blend-mode:multiply;background-color: #d2d2d2;"></div>';
+
+
+echo '<div class="container">';
+echo '<div class="row">';
+echo '<div class="col-12 text-white">';
+
+echo '<div class="text-center">';
+echo '<h2>' . $title . '</h2>';
+echo '<div class="divider"></div>';
+echo $content;
+echo '</div>';
+
+echo $code;
+
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</section>';
+endwhile; endif;
 
 
 // wp_enqueue_script('anime-cdn-js', '//cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js');
