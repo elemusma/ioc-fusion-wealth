4461d4412b025f8f1b15872bd518ba0bb404df19
Efrain Lemus <efraintlemusm@gmail.com>
2022-09-16 15:27:44 -0400

    bold bullet points and font changes

--- themes/insideoutcreative/templates/page-entrepreneur.php
+++ themes/insideoutcreative/templates/page-entrepreneur.php
@@ -94,9 +94,9 @@ echo '<div class="mr-3 pt-2" style="width:45px;">';
 echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>';
 echo '</div>';
 
-echo '<div>';
-echo '<strong>' . $title . '</strong>';
-echo '</div>';
+echo '<ul class="list-unstyled">';
+echo '<li>' . $title . '</li>';
+echo '</ul>';
 
 echo '</div>';
 
@@ -117,9 +117,9 @@ echo '<div class="mr-3 pt-2" style="width:45px;">';
 echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>';
 echo '</div>';
 
-echo '<div>';
-echo '<strong>' . $title . '</strong>';
-echo '</div>';
+echo '<ul class="list-unstyled">';
+echo '<li>' . $title . '</li>';
+echo '</ul>';
 
 echo '</div>';
 
